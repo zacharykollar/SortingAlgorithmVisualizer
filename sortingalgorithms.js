94ee5b4bfc2c1d970092f9e4d6d45625510d21ae
mediumcavalry <77861842+mediumcavalry@users.noreply.github.com>
2021-11-11 18:31:10 -0500

    Removed redundancy; <p> added

--- sortingalgorithms.js
+++ sortingalgorithms.js
@@ -1,36 +1,30 @@
-var arr= [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-function createArray(){
-    for(let i = 0; i < arr.length; i++){
-        arr[i] = Math.floor(Math.random()* 100 + 1);
-    }
+var arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+function createArray() {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = Math.floor(Math.random() * 100 + 1);
+  }
 }
 
-
-function yeet (){
-    createArray();
-    document.getElementById("button").innerHTML = arr[9].toString();
+function loadVisuals() {
+  createArray();
+  for (let i = 0; i < arr.length; i++) {
+    var b = document.createElement("p");
+    changeColGraphics(b, i);
+    b.classList.className = "worker";
+    b.id = "col" + i.toString();
+    document.body.appendChild(b);
+  }
 }
 
-function loadVisuals(){
-    createArray();
-    for(let i = 0; i < arr.length; i++){
-        var b = document.createElement("button");
-        changeButtonGraphics(b, i);
-        b.classList.className = "worker";
-        b.id = "button" + i.toString();
-        document.body.appendChild(b);
-    }
+function changeCols() {
+  createArray();
+  for (let i = 0; i < arr.length; i++) {
+    var b = document.getElementById("col" + i.toString());
+    changeColGraphics(b, i);
+  }
 }
 
-function changeButtons(){
-    createArray();
-    for(let i = 0; i < arr.length; i++){
-        var b = document.getElementById("button"+i.toString());
-        changeButtonGraphics(b, i);
-    }
-}
-
-function changeButtonGraphics(button, buttonid){
-    button.innerHTML = arr[buttonid.toString()];
-    button.style.minHeight = (arr[buttonid].toString() * 5) + "px";
+function changeColGraphics(col, colid) {
+  col.innerHTML = arr[colid.toString()];
+  col.style.minHeight = arr[colid].toString() * 5 + "px";
 }
