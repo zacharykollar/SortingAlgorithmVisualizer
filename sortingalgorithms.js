8f09afe83808e23591a3c2d512247cafefa8dc2b
mediumcavalry <77861842+mediumcavalry@users.noreply.github.com>
2021-11-15 19:22:55 -0500

    Cleaned up defunct code

--- sortingalgorithms.js
+++ sortingalgorithms.js
@@ -28,49 +28,29 @@ function loadVisuals() {
 
 function changeCols() {
   for (let i = 0; i < arr.length; i++) {
-    // setTimeout(() => {
     let b = document.getElementById("col" + i.toString());
     let temp = document.getElementById("col" + i.toString());
-
     changeColGraphics(b, i);
-
-    // }, 1500 * i);
   }
 }
 
 function changeColGraphics(col, colid) {
-  //setTimeout(() => {
   console.log("timeout");
   col.innerHTML = arr[colid].toString();
   col.style.height = (arr[colid] * 5 + 15).toString() + "px";
-  //}, 100 * colid);
-  //bubbleSort();
-  //sorted = false;
 }
 
-function bubbleSort(callback) {
-  //while (!sorted) {
-  //  sorted = true;
+function bubbleSort() {
   for (let i = 0; i < arr.length - 1; i++) {
     if (arr[i] > arr[i + 1]) {
-      //sorted = false;
-      console.log("madeit");
-      //setTimeout(()=>{callback(arr, i, i + 1)}, 250 * i);
       swap(arr, i, i + 1);
     }
   }
-  /*for (let i = 0; i < arr.length - 1; i++) {
-      if (arr[i] > arr[i + 1]) {
-        sorted = false;
-      }
-    }
-  }*/
 }
 
 function swap(list, first, second) {
   console.log("swapp" + first.toString());
   setTimeout(() => {
-    //console.log("timeout");
     let a = list[first];
     list[first] = list[second];
     list[second] = a;
