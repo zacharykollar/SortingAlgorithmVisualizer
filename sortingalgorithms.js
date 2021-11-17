0388d05c427aa4be7d0ab870ef3fe1bf67335e41
mediumcavalry <77861842+mediumcavalry@users.noreply.github.com>
2021-11-17 13:12:19 -0500

    Binary Search

--- sortingalgorithms.js
+++ sortingalgorithms.js
@@ -1,11 +1,67 @@
 var arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 var sorted = false;
 var sorts = 0;
-var timedelay = 150;
+var timedelay = 15;
 var defaultcolor = "hotpink";
 var highlightcolor = "blue";
 var comparecolor = "green";
 
+//these are the algorithms
+function binarySearch(array, x) {
+
+  if (sorted) {
+    let start=0, end=array.length-1;
+         
+    // Iterate while start not meets end
+    while (start<=end){
+ 
+        // Find the mid index
+        let mid=Math.floor((start + end)/2);
+  
+        // If element is present at mid, return True
+        if (arr[mid]===x) 
+          console.log(mid.toString() + ":" + x);
+ 
+        // Else look in left or right half accordingly
+        else if (arr[mid] < x)
+             start = mid + 1;
+        else
+             end = mid - 1;
+    }
+  
+  } else {
+    console.log("Sort before searching");
+  }
+}
+
+async function bubbleSort() {
+  while (sorts < arr.length - 1 && !sorted) {
+    sorted = true;
+    for (let i = 0; i < arr.length - (1 + sorts); i++) {
+      let compare1 = document.getElementById("col" + i.toString());
+      let compare2 = document.getElementById("col" + (i + 1).toString());
+      await new Promise((resolve) =>
+        setTimeout(() => {
+          if (arr[i] > arr[i + 1]) {
+            sorted = false;
+            swap(arr, i, i + 1);
+          } else {
+            highlight(comparecolor, compare1, compare2);
+            setTimeout(() => {
+              highlight(defaultcolor, compare1, compare2);
+            }, timedelay);
+          }
+          resolve();
+        }, timedelay)
+      );
+    }
+    sorts++;
+  }
+  console.log("done");
+  //binarySearch(arr, arr[Math.floor(Math.random() * 10)]);
+}
+
+//here down are tool functions/generics
 function createArray() {
   sorted = false;
   for (let i = 0; i < arr.length; i++) {
@@ -45,32 +101,6 @@ function changeColGraphics(col, colid) {
   col.style.height = (arr[colid] * 5 + 15).toString() + "px";
 }
 
-async function bubbleSort() {
-  while (sorts < arr.length - 1 && !sorted) {
-    sorted = true;
-    for (let i = 0; i < arr.length - (1 + sorts); i++) {
-      let compare1 = document.getElementById("col" + i.toString());
-      let compare2 = document.getElementById("col" + (i + 1).toString());
-      await new Promise((resolve) =>
-        setTimeout(() => {
-          if (arr[i] > arr[i + 1]) {
-            sorted = false;
-            swap(arr, i, i + 1);
-          } else {
-            highlight(comparecolor, compare1, compare2);
-            setTimeout(() => {
-              highlight(defaultcolor, compare1, compare2);
-            }, timedelay);
-          }
-          resolve();
-        }, timedelay)
-      );
-    }
-    sorts++;
-  }
-  console.log("done");
-}
-
 function swap(list, first, second) {
   let a = list[first];
   list[first] = list[second];
@@ -84,6 +114,7 @@ function swap(list, first, second) {
   }, timedelay);
   changeCols();
 }
+
 function highlight(color) {
   for (let a of arguments) {
     if (a.style) {
