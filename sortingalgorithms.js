02d89e4524cfe0786e9fcc374a288f6e9695f26b
mediumcavalry <77861842+mediumcavalry@users.noreply.github.com>
2021-11-17 22:56:43 -0500

    Added insertionsort
    Doesn't work very well and highlights are insane

--- sortingalgorithms.js
+++ sortingalgorithms.js
@@ -1,34 +1,38 @@
 var arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 var sorted = false;
 var sorts = 0;
-var timedelay = 15;
+var timedelay = 150;
 var defaultcolor = "hotpink";
 var highlightcolor = "blue";
 var comparecolor = "green";
-
+var searchval = document.getElementById("searchval");
 //these are the algorithms
-function binarySearch(array, x) {
-
+function binarySearch(array) {
+  let x = searchval.value;
   if (sorted) {
-    let start=0, end=array.length-1;
-         
+    let start = 0,
+      end = array.length - 1;
     // Iterate while start not meets end
-    while (start<=end){
- 
-        // Find the mid index
-        let mid=Math.floor((start + end)/2);
-  
-        // If element is present at mid, return True
-        if (arr[mid]===x) 
-          console.log(mid.toString() + ":" + x);
- 
-        // Else look in left or right half accordingly
-        else if (arr[mid] < x)
-             start = mid + 1;
-        else
-             end = mid - 1;
+    while (start <= end) {
+      // Find the mid index
+      let mid = Math.floor((start + end) / 2);
+      // If element is present at mid, return True
+      if (arr[mid] === x) {
+        console.log(mid.toString() + ":" + x);
+        highlight(
+          highlightcolor,
+          document.getElementById("col" + mid.toString())
+        );
+        return;
+      }
+      // Else look in left or right half accordingly
+      else if (arr[mid] < x) {
+        start = mid + 1;
+      } else {
+        end = mid - 1;
+      }
     }
-  
+    console.log("Does not Exist");
   } else {
     console.log("Sort before searching");
   }
@@ -59,6 +63,36 @@ async function bubbleSort() {
   }
   console.log("done");
   //binarySearch(arr, arr[Math.floor(Math.random() * 10)]);
+}
+
+async function insertionSort() {
+  while (sorts < arr.length - 1 && !sorted) {
+    //iterate through the array and find sorts
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] <= arr[i + 1]) {
+        sorts++;
+      } else {
+        console.log("breaking");
+        break;
+      }
+    }
+    for (let i = 0; i < sorts - 1; i++) {
+      highlight(comparecolor, document.getElementById("col" + (i).toString()));
+    }
+    console.log("unbroken");
+    for (let i = 0; i < arr.length - 1; i++) {
+      console.log("welcome");
+      if (arr[sorts] >= arr[i] && arr[sorts] <= arr[i + 1]) {
+        await new Promise ((resolve) =>
+        setTimeout(() => {
+          swap(arr, sorts, i);
+        console.log("swapped");
+        resolve();
+        }, timedelay));
+      }
+    }
+    sorts++;
+  }
 }
 
 //here down are tool functions/generics
